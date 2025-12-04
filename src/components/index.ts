1f9ce6af9d2453ed24691447f4d4f142d2d69fee
Bridge Homies <homiesbridge@gmail.com>
2025-12-04 19:44:37 +0500

    NextJS downgrade

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+// Export general components
+export { default as ErrorReporter } from "./ErrorReporter";
+
+// Export UI components via the ui folder
+export * from "./ui";
---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+// Export all UI components individually
+export { Button } from "./button";
+export { Input } from "./input";
+export { Card } from "./card";
+export { Checkbox } from "./checkbox";
+export { Select } from "./select";
+
+// Add any other UI components you have
